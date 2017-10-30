01d1babc5f8b5b0946e80c46b173fff4d58238c2
cyberpepe <kekronet@gmail.com>
2017-10-30 17:13:35 -0600

    MN 15 min confirmations

--- src/masternode.h
+++ src/masternode.h
@@ -27,10 +27,10 @@ class uint256;
 #define MASTERNODE_SYNC_IN_PROCESS             8
 #define MASTERNODE_REMOTELY_ENABLED            9
 
-#define MASTERNODE_MIN_CONFIRMATIONS           7
+#define MASTERNODE_MIN_CONFIRMATIONS           15
 #define MASTERNODE_MIN_DSEEP_SECONDS           (30*60)
 #define MASTERNODE_MIN_DSEE_SECONDS            (5*60)
-#define MASTERNODE_PING_SECONDS                (1*60)
+#define MASTERNODE_PING_SECONDS                (1l*60)
 #define MASTERNODE_EXPIRATION_SECONDS          (65*60)
 #define MASTERNODE_REMOVAL_SECONDS             (70*60)
 
