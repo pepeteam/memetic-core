cf283f07c0b22bb2e415661cac19e25d30333c08
cyberpepe <kekronet@gmail.com>
2019-12-22 19:51:01 -0700

    Add checkpoint at block 2239315

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -75,6 +75,7 @@ namespace Checkpoints
         (1833222, uint256("0xdd9c82e950acd29943da35de27193c77ab7d1d7c2b60e1b9fb6625d6a300e470"))
         (1839903, uint256("0x59154e986b1ddc7367b341ee6582818a4e7879fc9bb2f4c9a9eca036c29ab2f1"))
         (2000077, uint256("0xc2f2b32dd89d51891c03565e54fcb9f3a40a35b193ce5c3f54c19c19672e6e49"))
+        (2239315, uint256("0x4abc05174bd3004a435c50a13d5d212ac2c840ff97f00032ac3e03dacddf3844"))
     ;
 
     // TestNet has no checkpoints
